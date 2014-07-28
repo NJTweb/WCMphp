e740242f1ea73fdceef48bca751c200f668a2d3e
KthProg <kthprog@gmail.com>
2014-07-28 16:44:16 -0400

    Created query object, will integrate tomorrow.

---
+++ PHPWebProject1/scripts/js/query.js
@@ -0,0 +1,26 @@
+﻿function Query(name, params, fetchType, connection) {
+    this.results = {};
+    this.name = name;
+    this.params = params;
+    this.connection = connection;
+    this.fetchType = fetchType;
+    //fetchType can be "ASSOC" or "NUM"
+}
+
+Query.prototype.execute = function () {
+    ajaxPostJSONjQuery("scripts/php/Query.php", this, "storeResult", true);
+};
+
+Query.prototype.toString = function (func) {
+    // func should take a single object (result)
+    // return a string
+    var retStr = "";
+    for (var i = 0, l = this.results.length; i < l; ++i) {
+        retStr += func(this.results[i]);
+    }
+    return retStr;
+};
+
+Query.prototype.storeResult = function(data){
+    this.results = data;
+};
